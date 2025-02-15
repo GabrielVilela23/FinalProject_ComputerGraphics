4c6ee3577be582189c77895c52458adbb3000c27
Gabriel Santos de Sousa <gablsimportante@gmail.com>
2025-02-15 10:47:48 -0300

    feat: create sphereHud

--- src/js/hud.js
+++ src/js/hud.js
@@ -13,14 +13,25 @@ export function healthHud(player) {
     }
 }
 
+export function sphereHud(player) {
+    const pElement = document.createElement('p');
+    pElement.innerHTML = 'Esferas: ' + (player.spheres).toString().padStart(2, '0');
+    
+    const spheresCounter = document.getElementById('spheres-hud');
+    spheresCounter.innerHTML = '';
+    spheresCounter.appendChild(pElement);
+}
+
 export function createHud(player) {
    healthHud(player);
+   sphereHud(player);
 }
 
 if (prod) {
     document.addEventListener('keydown', function (event) {
         if (event.key === ' ' || event.code === 'Space') {
-            window.player.takeDamage(1);
+            // window.player.takeDamage(1);
+            window.player.collectSpheres();
         }
     });
 }
