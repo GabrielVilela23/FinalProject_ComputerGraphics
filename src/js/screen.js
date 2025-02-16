b9143cdba719b5b2680dd953b656a7c21a0e5f77
Livia Canuto <liviacanuto01@gmail.com>
2025-02-16 18:11:14 -0300

    add: screens

---
+++ src/js/screen.js
@@ -0,0 +1,55 @@
+export class Screen {
+    constructor() {
+      this.menuScreen = document.getElementById('menu');
+      this.winScreen = document.getElementById('win-game');
+      this.gameOverScreen = document.getElementById('game-over');
+      this.startButton = document.getElementById('start-btn');
+      this.gameOverButton = document.getElementById('game-over-btn');
+      this.winGameButton = document.getElementById('win-btn');
+      this.initListeners();
+    }
+  
+    initListeners() {
+      if (this.startButton) {
+        this.startButton.addEventListener('click', () => this.onStart());
+      }
+      if(this.gameOverButton) {
+        this.gameOverButton.addEventListener('click', () => this.onStart());
+      }
+      if(this.winGameButton) {
+        this.winGameButton.addEventListener('click', () => this.onStart());
+      }
+    }
+  
+    showScreen(screen) {
+      this.hideAllScreens();
+      if (screen) {
+        screen.classList.remove('hidden');
+      }
+    }
+  
+    hideAllScreens() {
+      this.menuScreen.classList.add('hidden');
+      this.winScreen.classList.add('hidden');
+      this.gameOverScreen.classList.add('hidden');
+    }
+  
+    showMenu() {
+      this.showScreen(this.menuScreen);
+    }
+  
+    showWinScreen() {
+      this.showScreen(this.winScreen);
+    }
+  
+    showGameOverScreen() {
+      this.showScreen(this.gameOverScreen);
+    }
+  
+    onStart() {
+      console.log('Game started!');
+      this.hideAllScreens();
+      const startEvent = new CustomEvent('startGame');
+      document.dispatchEvent(startEvent);
+    }
+}
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,39 +5,111 @@ import { checkCollisions } from './collision.js';
 import { addModel } from './modelLoader.js';
 import { objPlayer } from './player.js';
 import { createHud } from './hud.js';
+import { Screen } from './screen.js';
 import * as THREE from 'three';
 
-// Lógica do Jogo
-window.prod = false;
-window.player = new objPlayer();
-createHud(window.player);
+const screen = new Screen();
 
-// Renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xa3a3a3);
-document.body.appendChild(renderer.domElement);
+document.addEventListener('startGame', () => {
+    startGame();
+});
 
-// Cena
-const scene = new THREE.Scene();
+screen.showMenu();
 
-// Camera
-const P0 = [10, 10, 10];
-const P_ref = [2, 1, 1];
-const V = [0, 1, 0];
-const camera = new Camera(P0, P_ref, V, renderer.domElement);
+let renderer;
+let clock;
+let mixers;
+let scene;
+let camera;
+let orbit;
 
-// Orbitais
-const orbit = new OrbitControls(camera.three, renderer.domElement);
-orbit.update();
+function startGame() {
+    console.log('WebGL game started!');
+    initGame();
+}
 
-/// Loop de animação
-const clock = new THREE.Clock();
+function initGame() {
+    // Lógica do Jogo
+    window.prod = false;
+    window.player = new objPlayer();
+    createHud(window.player);
+
+    // Renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0xa3a3a3);
+    document.body.appendChild(renderer.domElement);
+
+    // Cena
+    scene = new THREE.Scene();
+
+    // Camera
+    const P0 = [10, 10, 10];
+    const P_ref = [2, 1, 1];
+    const V = [0, 1, 0];
+    camera = new Camera(P0, P_ref, V, renderer.domElement);
+
+    // Orbitais
+    orbit = new OrbitControls(camera.three, renderer.domElement);
+    orbit.update();
+
+    /// Loop de animação
+
+    // Adicionar luzes à cena
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1); // Luz ambiente
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 2); // Luz direcional
+    directionalLight.position.set(10, 10, 10);
+    scene.add(directionalLight);
+
+    // Endereços dos modelos
+    const dragon = new URL('../assets/eastern_dragon.glb', import.meta.url);
+    const donut = new URL('../assets/Donut.glb', import.meta.url);
+    const candy = new URL('../assets/CandyCane.glb', import.meta.url);
+    const chocolate = new URL('../assets/ChocolateBar.glb', import.meta.url);
+    const oreo = new URL('../assets/Oreos.glb', import.meta.url);
+
+    // Array global para armazenar mixers
+    mixers = [];
+
+    // Adicionar modelos à cena com diferentes escalas
+    addModel(scene, mixers, dragon.href, { x: 0, y: 0, z: 0 }, { x: 1, y: 1, z: 1 }, 'dragon');
+    addModel(scene, mixers, donut.href, { x: -5, y: 0, z: 11 }, { x: 3, y: 3, z: 3 }, 'collectible');
+    addModel(scene, mixers, donut.href, { x: -10, y: 0, z: 11 }, { x: 3, y: 3, z: 3 }, 'collectible');
+    addModel(scene, mixers, donut.href, { x: -15, y: 0, z: 11 }, { x: 3, y: 3, z: 3 }, 'collectible');
+    addModel(scene, mixers, donut.href, { x: -20, y: 0, z: 11 }, { x: 3, y: 3, z: 3 }, 'collectible');
+    addModel(scene, mixers, donut.href, { x: -25, y: 0, z: 11 }, { x: 3, y: 3, z: 3 }, 'collectible');
+
+    addModel(scene, mixers, oreo.href, { x: 5, y: 0, z: 11 }, { x: 10, y: 10, z: 10 }, 'oreo');
+    addModel(scene, mixers, oreo.href, { x: 10, y: 0, z: 11 }, { x: 10, y: 10, z: 10 }, 'oreo');
+    addModel(scene, mixers, oreo.href, { x: 15, y: 0, z: 11 }, { x: 10, y: 10, z: 10 }, 'oreo');
+    addModel(scene, mixers, oreo.href, { x: 20, y: 0, z: 11 }, { x: 10, y: 10, z: 10 }, 'oreo');
+    addModel(scene, mixers, oreo.href, { x: 25, y: 0, z: 11 }, { x: 10, y: 10, z: 10 }, 'oreo');
+
+    renderer.setAnimationLoop(animate);
+}
+
 function animate() {
+    if (player.health <= 0) {
+        renderer.setAnimationLoop(null);
+        let canvas = document.querySelector('canvas');
+        canvas.remove();
+        screen.showGameOverScreen();
+    }
+
+    if (player.spheres >= 5) {
+        renderer.setAnimationLoop(null);
+        let canvas = document.querySelector('canvas');
+        canvas.remove();
+        screen.showWinScreen();
+    }
+
+    clock = new THREE.Clock();
     const delta = clock.getDelta();
 
     mixers.forEach((mixer) => mixer.update(delta));
-    smoothRotation();  // Chama a suavização da rotação
+    smoothRotation(); // Chama a suavização da rotação
 
     // Atualização: Orbitais
     orbit.update();
@@ -46,7 +118,7 @@ function animate() {
     updateCameraPosition(scene, camera);
 
     // Atulização: Posição da caixa de colissão dos modelos
-    scene.traverse(child => {
+    scene.traverse((child) => {
         if (child.isObject3D && child.userData.updateBoundingBox) {
             child.userData.updateBoundingBox();
         }
@@ -59,60 +131,23 @@ function animate() {
     renderer.render(scene, camera.three);
 }
 
-
-
-
-
-// Adicionar luzes à cena
-const ambientLight = new THREE.AmbientLight(0xffffff, 1); // Luz ambiente
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2); // Luz direcional
-directionalLight.position.set(10, 10, 10);
-scene.add(directionalLight);
-
-// Endereços dos modelos
-const dragon = new URL('../assets/eastern_dragon.glb', import.meta.url);
-const donut = new URL('../assets/Donut.glb', import.meta.url);
-const candy = new URL('../assets/CandyCane.glb', import.meta.url);
-const chocolate = new URL('../assets/ChocolateBar.glb', import.meta.url);
-const oreo = new URL('../assets/Oreos.glb', import.meta.url);
-
-// Array global para armazenar mixers
-const mixers = [];
-
-// Adicionar modelos à cena com diferentes escalas
-addModel(scene, mixers, dragon.href, { x: 0, y: 0, z: 0 }, { x: 1, y: 1, z: 1 }, "dragon");
-addModel(scene, mixers, donut.href, { x: -5, y: 0, z: 11 }, { x: 3, y: 3, z: 3 }, "collectible");
-addModel(scene, mixers, donut.href, { x: -10, y: 0, z: 11 }, { x: 3, y: 3, z: 3 }, "collectible");
-addModel(scene, mixers, donut.href, { x: -15, y: 0, z: 11 }, { x: 3, y: 3, z: 3 }, "collectible");
-addModel(scene, mixers, donut.href, { x: -20, y: 0, z: 11 }, { x: 3, y: 3, z: 3 }, "collectible");
-addModel(scene, mixers, donut.href, { x: -25, y: 0, z: 11 }, { x: 3, y: 3, z: 3 }, "collectible");
-
-addModel(scene, mixers, oreo.href, { x: 5, y: 0, z: 11 }, { x: 10, y: 10, z: 10 }, "oreo");
-addModel(scene, mixers, oreo.href, { x: 10, y: 0, z: 11 }, { x: 10, y: 10, z: 10 }, "oreo");
-addModel(scene, mixers, oreo.href, { x: 15, y: 0, z: 11 }, { x: 10, y: 10, z: 10 }, "oreo");
-addModel(scene, mixers, oreo.href, { x: 20, y: 0, z: 11 }, { x: 10, y: 10, z: 10 }, "oreo");
-addModel(scene, mixers, oreo.href, { x: 25, y: 0, z: 11 }, { x: 10, y: 10, z: 10 }, "oreo");
-
-
 // Funções de movimentação do dragão
 function moveDragonX(distance) {
-    const dragon = scene.getObjectByName("dragon");  // Buscar o modelo pelo nome
+    const dragon = scene.getObjectByName('dragon'); // Buscar o modelo pelo nome
     if (dragon) {
         dragon.position.x += distance;
     }
 }
 
 function moveDragonY(distance) {
-    const dragon = scene.getObjectByName("dragon");  // Buscar o modelo pelo nome
+    const dragon = scene.getObjectByName('dragon'); // Buscar o modelo pelo nome
     if (dragon) {
         dragon.position.y += distance;
     }
 }
 
 function moveDragonZ(distance) {
-    const dragon = scene.getObjectByName("dragon");  // Buscar o modelo pelo nome
+    const dragon = scene.getObjectByName('dragon'); // Buscar o modelo pelo nome
     if (dragon) {
         dragon.position.z += distance;
     }
@@ -120,19 +155,18 @@ function moveDragonZ(distance) {
 
 // Funções de rotação do dragão
 function rotateDragonY(angle) {
-    const dragon = scene.getObjectByName("dragon");  // Buscar o modelo pelo nome
+    const dragon = scene.getObjectByName('dragon'); // Buscar o modelo pelo nome
     if (dragon) {
         dragon.rotation.y += angle;
     }
 }
 
 function rotateDragonX(angle) {
-    const dragon = scene.getObjectByName("dragon");  // Buscar o modelo pelo nome
+    const dragon = scene.getObjectByName('dragon'); // Buscar o modelo pelo nome
     if (dragon) {
         dragon.rotation.x += angle;
     }
 }
-
 
 // Responsividade: Redimensionar tela
 window.addEventListener('resize', function () {
@@ -142,26 +176,26 @@ window.addEventListener('resize', function () {
 });
 
 // Variáveis para controlar a rotação
-let targetRotation = 0;  // Rotações alvo para o dragão
+let targetRotation = 0; // Rotações alvo para o dragão
 const rotationSpeed = 0.05; // Velocidade da rotação suave
 
 document.addEventListener('keydown', (event) => {
     const step = 0.3; // Passo de movimento
 
-    const dragon = scene.getObjectByName("dragon");  // Buscar o modelo do dragão
+    const dragon = scene.getObjectByName('dragon'); // Buscar o modelo do dragão
     if (!dragon) return;
 
     switch (event.key) {
-        case 'w':  // Mover para frente
+        case 'w': // Mover para frente
             dragon.position.z += step;
             break;
-        case 's':  // Mover para trás
+        case 's': // Mover para trás
             dragon.position.z -= step;
             break;
-        case 'a':  // Mover para a esquerda
+        case 'a': // Mover para a esquerda
             dragon.position.x += step;
             break;
-        case 'd':  // Mover para a direita
+        case 'd': // Mover para a direita
             dragon.position.x -= step;
             break;
     }
@@ -169,7 +203,7 @@ document.addEventListener('keydown', (event) => {
 
 // Função para suavizar a rotação do dragão
 function smoothRotation() {
-    const dragon = scene.getObjectByName("dragon");
+    const dragon = scene.getObjectByName('dragon');
     if (!dragon) return;
 
     // Fazendo a rotação de forma suave
@@ -179,4 +213,3 @@ function smoothRotation() {
     dragon.rotation.y += sign * rotationAmount;
 }
 
-renderer.setAnimationLoop(animate);
