d4728ee56e6fdb76eab8a090753cd7c6db197b3d
Gabriel Santos de Sousa <gablsimportante@gmail.com>
2025-02-16 12:35:37 -0300

    feat: remove other model when player collide

--- src/js/collision.js
+++ src/js/collision.js
@@ -1,23 +1,42 @@
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import * as THREE from 'three';
-
 export function checkCollision(box1, box2) {
     return box1.intersectsBox(box2);
 }
 
 export function checkCollisions(scene) {
     const models = scene.children.filter(child => child.isObject3D && child.userData.boundingBox);
+    
     for (let i = 0; i < models.length; i++) {
         for (let j = i + 1; j < models.length; j++) {
             const box1 = models[i].userData.boundingBox;
             const box2 = models[j].userData.boundingBox;
 
-            if (box1 && box2) {
-                if (box1.intersectsBox(box2)) {
-                    console.log(`Colisão detectada entre ${models[i].name} e ${models[j].name}`);
+            if (box1 && box2 && box1.intersectsBox(box2)) {
+                if (models[i].name === 'collectible' || models[j].name === 'collectible'){
+                    window.player.collectSpheres();
                 }
-            } else {
-                console.warn('Bounding box não encontrada para um dos modelos:', models[i].name, models[j].name);
+                else {
+                    window.player.takeDamage(1);
+                }
+
+                if (models[i].name !== 'dragon') {
+                    scene.remove(models[i]);
+                    if (models[i].userData.boxHelper) {
+                        scene.remove(models[i].userData.boxHelper);
+                        delete models[i].userData.boxHelper;
+                    }
+                    models.splice(i, 1);
+                    i--;
+                    break;
+                } 
+                else if (models[j].name !== 'dragon') {
+                    scene.remove(models[j]);
+                    if (models[j].userData.boxHelper) {
+                        scene.remove(models[j].userData.boxHelper);
+                        delete models[j].userData.boxHelper;
+                    }
+                    models.splice(j, 1);
+                    j--;
+                }
             }
         }
     }
